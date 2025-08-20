e5bf57e43766576fd35c17043b48d685e7a6030b
Medy <mannomedy@gmail.com>
2025-08-20 15:15:21 +0300

    add changes

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
-    pass
+    """
+    Creates a new .txt file (or overwrites if it exists) 
+    and writes file_content into it.
+    """
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Opens the .txt file in append mode and adds new content.
+    """
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Opens the .txt file in read mode and returns its contents as a string.
+    """
+    with open(f"{file_name}.txt", "r") as f:
+        return f.read()
